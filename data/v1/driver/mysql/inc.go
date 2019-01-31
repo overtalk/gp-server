f21881c6cf2e3ac85da8e30459fcc1ca02cf5560
秦翰 <qinhan@xindong.com>
2019-01-31 18:53:52 +0800

    fix(data):fix a bug in data/inc

--- data/v1/driver/mysql/inc.go
+++ data/v1/driver/mysql/inc.go
@@ -2,11 +2,13 @@ package driver
 
 import (
 	"strings"
+
+	"github.com/QHasaki/Server/logger"
 )
 
 func Inc(p *Pool, document string, columns []string, where Data) error {
 	var (
-		datas []string
+		datas  []string
 		column []string
 		needed []string
 		values []interface{}
@@ -15,21 +17,21 @@ func Inc(p *Pool, document string, columns []string, where Data) error {
 	sql := "INSERT INTO `" + document + "` "
 
 	for k, v := range where {
-		column = append(column, "`" + k + "`")
-		needed = append(needed,"?")
+		column = append(column, "`"+k+"`")
+		needed = append(needed, "?")
 		values = append(values, v)
 	}
 
 	for _, v := range columns {
-		column = append(column, "`" + v + "`")
-		needed = append(needed,"?")
+		column = append(column, "`"+v+"`")
+		needed = append(needed, "?")
 		values = append(values, 1)
-		datas = append(datas, "`" + v + "` = `" + v + "`+1")
+		datas = append(datas, "`"+v+"` = `"+v+"`+1")
 	}
 
-	resp, err := p.Query(sql + "(" + strings.Join(column, ",") + ") VALUES (" + strings.Join(needed,", ") + ") ON DUPLICATE KEY UPDATE " + strings.Join(datas, ", "), values...)
+	resp, err := p.Query(sql+"("+strings.Join(column, ",")+") VALUES ("+strings.Join(needed, ", ")+") ON DUPLICATE KEY UPDATE "+strings.Join(datas, ", "), values...)
 	if err != nil {
-		sugar.Errorf("failed to query row : %v", err)
+		logger.Sugar.Errorf("failed to query row : %v", err)
 		return err
 	}
 
