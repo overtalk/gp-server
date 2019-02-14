325861148caf37b0871f90e316196e90550232a3
秦翰 <qinhan@xindong.com>
2019-02-14 12:08:58 +0800

    test(service/db/driver):add mysql driver test

--- service/db/v1/driver/mysql/init_test.go
+++ service/db/v1/driver/mysql/init_test.go
@@ -6,20 +6,24 @@ import (
 	"github.com/QHasaki/Server/service/db/v1/driver/mysql"
 )
 
-func TestNewMysqlDriver(t *testing.T) {
+func getMySqlDriver(t *testing.T) *driver.MysqlDriver {
 	dbInfo := &driver.MysqlDBInfo{
 		MaxConnection: 2000,
-		Addr:          "172.26.32.12",
-		Username:      "sausage",
-		Password:      "sausage_shooter",
+		Addr:          "localhost",
+		Username:      "root",
+		Password:      "12345678",
 		DBName:        "sausage_shooter",
 	}
 	mysqlDriver, err := driver.NewMysqlDriver(dbInfo)
 	if err != nil {
 		t.Errorf("failed to new mysql driver : %v", err)
-		return
+		return nil
 	}
+	return mysqlDriver
+}
 
+func TestNewMysqlDriver(t *testing.T) {
+	mysqlDriver := getMySqlDriver(t)
 	if err := mysqlDriver.Ping(); err != nil {
 		t.Errorf("failed to ping mysql : %v", err)
 		return
---
+++ service/db/v1/driver/mysql/utils_test.go
@@ -0,0 +1,106 @@
+package driver_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/QHasaki/Server/utils/parse"
+)
+
+func TestQuery(t *testing.T) {
+	mysqlDriver := getMySqlDriver(t)
+
+	sql := "select `id`,`nickname` from `player` where `id` < ?"
+	gender := "10"
+
+	queryResp, err := mysqlDriver.Query(sql, gender)
+	if err != nil {
+		t.Errorf("failed to Query : %v", err)
+		return
+	}
+
+	rows := queryResp.GetRows()
+	defer queryResp.Close()
+
+	for rows.Next() {
+		rowColumns, err := rows.Columns()
+		if err != nil {
+			t.Errorf("failed to get Columns : %v", err)
+			return
+		}
+
+		rowColumnCount := len(rowColumns)
+		scanAddr := make([]interface{}, rowColumnCount)
+		scanResult := make([]interface{}, rowColumnCount)
+
+		for i, _ := range scanAddr {
+			scanAddr[i] = &scanResult[i]
+		}
+
+		if err = rows.Scan(scanAddr...); err != nil {
+			t.Errorf("failed to scan Columns : %v", err)
+			return
+		}
+
+		t.Logf("id = %d, nickname = %s", parse.Int(scanResult[0]), parse.String(scanResult[1]))
+	}
+}
+
+func TestQueryRow(t *testing.T) {
+	mysqlDriver := getMySqlDriver(t)
+
+	sql := "select `id`,`nickname` from `player` where `id` < ?"
+	gender := "10"
+
+	row := mysqlDriver.QueryRow(sql, gender)
+
+	rowColumnCount := 2
+	scanAddr := make([]interface{}, rowColumnCount)
+	scanResult := make([]interface{}, rowColumnCount)
+
+	for i, _ := range scanAddr {
+		scanAddr[i] = &scanResult[i]
+	}
+
+	if err := row.Scan(scanAddr...); err != nil {
+		t.Errorf("failed to scan Columns : %v", err)
+		return
+	}
+
+	t.Logf("id = %d, nickname = %s", parse.Int(scanResult[0]), parse.String(scanResult[1]))
+
+}
+
+func TestExec(t *testing.T) {
+	mysqlDriver := getMySqlDriver(t)
+
+	filed := "CUP_SEASON"
+	var season int64
+
+	querySql := "select `value` from `config` where `name` = ?"
+	row := mysqlDriver.QueryRow(querySql, filed)
+	if err := row.Scan(&season); err != nil {
+		t.Errorf("failed to scan : %v", err)
+		return
+	}
+
+	execSql := "UPDATE `config` SET `value` = ? WHERE `name` = ?"
+	args := []interface{}{season + 1, filed}
+
+	if err := mysqlDriver.Exec(execSql, args...); err != nil {
+		t.Errorf("failed to exec : %v", err)
+		return
+	}
+
+	row = mysqlDriver.QueryRow(querySql, filed)
+	if err := row.Scan(&season); err != nil {
+		t.Errorf("failed to scan : %v", err)
+		return
+	}
+
+	if !assert.Equal(t, season, parse.Int(args[0])) {
+		t.Error("failed to exec")
+		return
+	}
+}
