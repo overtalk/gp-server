97fa7c92d55333b6a2fb4bb9a58322fd14c8f51a
秦翰 <qinhan@xindong.com>
2019-02-27 17:02:07 +0800

    refactor(example):modify the method of register module to gate

--- service/example/init.go
+++ service/example/init.go
@@ -7,21 +7,19 @@ import (
 
 // Example : 业务逻辑模块的实现
 type Example struct {
-	gate module.Gate
 }
 
 // NewExampleModule : Example模块的构造函数
-func NewExampleModule(gate module.Gate) module.Example {
-	return &Example{
-		gate: gate,
-	}
+func NewExampleModule() module.Example {
+	return &Example{}
 }
 
 // Register : Example模块的注册函数，将改模块实现的功能注册到gate中
-func (t *Example) Register() {
-	t.gate.RegisterRoute("/example", module.Router{
+func Register(gate module.Gate) {
+	exampleModule := NewExampleModule()
+	gate.RegisterRoute("/example", module.Router{
 		Method:  "POST",
-		Handler: t.Example,
+		Handler: exampleModule.Example,
 	})
 }
 
