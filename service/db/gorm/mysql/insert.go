2d454d2c489b3929a025fcd19890f06bbf9f1bc9
秦翰 <qinhan@xindong.com>
2019-02-27 14:57:23 +0800

    revert(service/db/gorm/mysql):del insert.go

--- service/db/gorm/mysql/insert.go
+++
@@ -1,7 +0,0 @@
-package db
-
-// Insert : 插入数据，传入model即可（value）
-// value 为struct的指针，插入成功之后，value中未赋值的字段将会被赋值
-func (m *MysqlDriver) Insert(value interface{}) error {
-	return m.conn.Create(value).Error
-}
