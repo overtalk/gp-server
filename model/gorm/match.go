6f70b0c8321bfb8c4d3a0fccd16c99228fabcf77
秦翰 <qinhan@xindong.com>
2019-03-04 16:25:16 +0800

    feat(model/gorm):add match model

---
+++ model/gorm/match.go
@@ -0,0 +1,11 @@
+package model
+
+// Match : table `match`
+type Match struct {
+	ID             int    `gorm:"type : int(20); primary_key; not null; auto_increment"`
+	PaperID        int    `gorm:"type : int(20); not null"`
+	InvitationCode string `gorm:"type :  varchar(50); not null; unique"`
+	IsPublic       bool   `gorm:"type : boolean; not null; default : 1"`
+	StartTime      int64  `gorm:"type :  int(64); not null"`
+	Duration       int64  `gorm:"type :  int(20); not null"`
+}
