601cd0c693f00c56908e743bf013f130f05ae0af
秦翰 <qinhan@xindong.com>
2019-03-05 11:14:49 +0800

    refactor(service/example): modify example service

--- module/example.go
+++ module/example.go
@@ -2,5 +2,5 @@ package module
 
 // Example : 业务逻辑模块demo
 type Example interface {
-	Example(data interface{}) interface{}
+	Example(data ...interface{}) interface{}
 }
--- service/example/init.go
+++ service/example/init.go
@@ -1,8 +1,12 @@
 package example
 
 import (
+	"github.com/golang/protobuf/proto"
+
+	"github.com/qinhan-shu/gp-server/logger"
 	"github.com/qinhan-shu/gp-server/module"
 	"github.com/qinhan-shu/gp-server/protocol"
+	"github.com/qinhan-shu/gp-server/utils/parse"
 )
 
 // Example : 业务逻辑模块的实现
@@ -24,9 +28,26 @@ func Register(gate module.Gate) {
 }
 
 // Example : Example模块的业务逻辑处理函数
-func (t *Example) Example(data interface{}) interface{} {
-	return &protocol.TestResponse{
-		A: "111",
-		B: "111",
+func (t *Example) Example(data ...interface{}) interface{} {
+	req := &protocol.TestRequest{}
+	resp := &protocol.TestResponse{}
+	if len(data) != 2 {
+		resp.A = "no token"
+		return resp
 	}
+
+	token := parse.String(data[0])
+	logger.Sugar.Infof("get token : %s", token)
+
+	if err := proto.Unmarshal(parse.Bytes(data[1]), req); err != nil {
+		logger.Sugar.Errorf("failed to unmarshal : %v", err)
+		resp.A = err.Error()
+		return resp
+	}
+
+	logger.Sugar.Infof("request = %v", req)
+	resp.A = "all ok"
+	resp.B = "example"
+
+	return resp
 }
