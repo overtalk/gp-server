48a7b5749fe6ca97d13b41a5ca1d679617f230eb
秦翰 <qinhan@xindong.com>
2019-03-05 23:37:39 +0800

    feat(utils/parse/int): add intwitherror func

--- utils/parse/int.go
+++ utils/parse/int.go
@@ -9,6 +9,15 @@ import (
 
 // Int turn ( interface{} ) to ( int64 )
 func Int(in interface{}) int64 {
+	out, err := IntWithError(in)
+	if err != nil {
+		out = 0
+	}
+	return out
+}
+
+// IntWithError turn ( interface{} ) to ( int64 ) with error if error
+func IntWithError(in interface{}) (int64, error) {
 	var ret int64
 	jsonIn, ok := in.(json.Number)
 	if ok {
@@ -18,23 +27,23 @@ func Int(in interface{}) int64 {
 	case []uint8:
 		inp := string(in.([]uint8))
 		if inp == "" {
-			return 0
+			return 0, Err{method: "Int", origin: in}
 		}
 		left, err := strconv.ParseInt(inp, 10, 64)
 		if err != nil {
 			logger.Sugar.Errorf("parse to int error([]uint8) : %v", err)
-			return ret
+			return 0, err
 		}
 		ret = left
 	case string:
 		inp := in.(string)
 		if inp == "" {
-			return 0
+			return 0, Err{method: "Int", origin: in}
 		}
 		left, err := strconv.ParseInt(inp, 10, 64)
 		if err != nil {
 			logger.Sugar.Errorf("parse to int error(string) : %v", err)
-			return ret
+			return 0, err
 		}
 		ret = left
 	case int:
@@ -54,11 +63,11 @@ func Int(in interface{}) int64 {
 	case float64:
 		ret = int64(in.(float64))
 	case nil:
-		return 0
+		return 0, Err{method: "Int", origin: in}
 	default:
 		logger.Sugar.Error("parse to int error : unknown type")
-		return 0
+		return 0, Err{method: "Int", origin: in}
 	}
 
-	return ret
+	return ret, nil
 }
