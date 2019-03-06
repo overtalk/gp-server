5316c6984daa8aeeaa13aab3f6ba1ebc6f704a7c
秦翰 <qinhan@xindong.com>
2019-03-06 14:51:37 +0800

    refactor(service/gate): modify service handler input args format

--- service/gate/gin/route.go
+++ service/gate/gin/route.go
@@ -31,23 +31,23 @@ func (s *Service) registerToGate() {
 		case "POST":
 			{
 				s.gin.POST(router, func(c *gin.Context) {
-					var inputArgs []interface{}
-
+					args := make(map[string]interface{})
+					// get `token`
 					cookie, err := c.Request.Cookie("token")
 					if err != nil {
 						logger.Sugar.Infof("failed to get token : %v", err)
 					}
 					if cookie != nil {
-						inputArgs = append(inputArgs, cookie.Value)
+						args[module.Token] = cookie.Value
 					}
-
+					// get request []bytes
 					data, err := ioutil.ReadAll(c.Request.Body)
 					if err != nil {
 						logger.Sugar.Errorf("failed to get body : %v", err)
 					}
-					inputArgs = append(inputArgs, data)
+					args[module.Request] = data
 
-					resp := handler.Handler(inputArgs...)
+					resp := handler.Handler(args)
 					logger.Sugar.Debugf("a post request for router [%s], response : %v", router, resp)
 					// 目前使用protobuf作为通信协议
 					// 由于gin框架支持protbuf，因此所有handler的resp都返回proto.Message,序列化由框架内部完成
