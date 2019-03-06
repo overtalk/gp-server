131a9e70ae3bbca3dffa906d3eb92bdd8eb1dd17
秦翰 <qinhan@xindong.com>
2019-03-06 15:49:03 +0800

    refactor(service/cache/redis): modify key

--- service/cache/redis/key.go
+++ service/cache/redis/key.go
@@ -7,11 +7,11 @@ import (
 // getTokenKey is to get to get key for the pair (userID-token)
 // the key is to used to make sure the uniqueness pair for userID and token
 func (r *RedisCache) getUserIDToTokenKey(userID string) string {
-	return fmt.Sprintf("user_t_%s", userID)
+	return fmt.Sprintf("u_%s", userID)
 }
 
 // getTokenToUserIDKey is to get key for the pair (token-userID)
 // the key is to used to authenticate the token, and get userID
 func (r *RedisCache) getTokenToUserIDKey(token string) string {
-	return fmt.Sprintf("token_p_%s", token)
+	return fmt.Sprintf("t_%s", token)
 }
