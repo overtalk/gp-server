180ec3a129a609b7fc8a425f88999b1aaf009cc4
秦翰 <qinhan@xindong.com>
2019-03-13 22:14:19 +0800

    feat(module/db): add GetUsersByRole interface and achievement

--- module/db.go
+++ module/db.go
@@ -6,7 +6,12 @@ import (
 
 // DB : database module
 type DB interface {
+	// auth
 	CheckPlayer(username, password string) (*model.User, error)
 	GetUserByID(id int) (*model.User, error)
+
+	// user manage
+	GetUsersByRole(role int64) ([]*model.User, error) // role < 0 : get all user
+
 	GetMatchByID(id int) (*model.Match, error)
 }
--- service/db/gorm/mysql/user.go
+++ service/db/gorm/mysql/user.go
@@ -1,35 +1,42 @@
 package db
 
 import (
+	// "github.com/jinzhu/gorm"
 	"github.com/qinhan-shu/gp-server/model/gorm"
 )
 
 // GetUserByID : get uer model by user id
 func (m *MysqlDriver) GetUserByID(id int) (*model.User, error) {
-	db := m.conn.First(&model.User{}, id)
-	if db.Error != nil {
-		return nil, db.Error
-	}
-
-	user := &model.User{}
-	if err := db.Scan(user).Error; err != nil {
+	var user model.User
+	if err := m.conn.First(&user, id).Error; err != nil {
 		return nil, err
 	}
 
-	return user, nil
+	return &user, nil
 }
 
 // CheckPlayer : get uer model by username and password
 func (m *MysqlDriver) CheckPlayer(username, password string) (*model.User, error) {
-	db := m.conn.Where("username = ? and password = ?", username, password).First(&model.User{})
-	if db.Error != nil {
-		return nil, db.Error
-	}
-
-	user := &model.User{}
-	if err := db.Scan(user).Error; err != nil {
+	var user model.User
+	if err := m.conn.Where("username = ? and password = ?", username, password).First(&user).Error; err != nil {
 		return nil, err
 	}
+	return &user, nil
+}
 
-	return user, nil
+// GetUsersByRole : get users by required role
+func (m *MysqlDriver) GetUsersByRole(role int64) ([]*model.User, error) {
+	var users []*model.User
+	var err error
+	if role < 0 {
+		// role < 0 : get all users
+		err = m.conn.Find(&users).Error
+	} else {
+		err = m.conn.Where("role = ? ", role).Find(&users).Error
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
