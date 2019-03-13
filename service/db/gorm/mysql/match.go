2fe2f98d9590b30067f5bdd8b12aada03b5c628f
秦翰 <qinhan@xindong.com>
2019-03-13 22:18:38 +0800

    refactor(service/db/gorm/mysql): modify GetMatchByID

--- service/db/gorm/mysql/match.go
+++ service/db/gorm/mysql/match.go
@@ -6,15 +6,9 @@ import (
 
 // GetMatchByID : get uer model by match id
 func (m *MysqlDriver) GetMatchByID(id int) (*model.Match, error) {
-	db := m.conn.First(&model.Match{}, id)
-	if db.Error != nil {
-		return nil, db.Error
-	}
-
-	match := &model.Match{}
-	if err := db.Scan(match).Error; err != nil {
+	var match model.Match
+	if err := m.conn.First(&match, id).Error; err != nil {
 		return nil, err
 	}
-
-	return match, nil
+	return &match, nil
 }
