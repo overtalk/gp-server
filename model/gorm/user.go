34e7587514c2d7f9ebfaebe638d583a17a59b34d
秦翰 <qinhan@xindong.com>
2019-03-14 00:49:50 +0800

    feat(model/user): add TurnProto of user

--- model/gorm/user.go
+++ model/gorm/user.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"github.com/qinhan-shu/gp-server/protocol"
+)
+
 // User : teble `user`
 type User struct {
 	ID            int64  `gorm:"type : int(20); primary_key; not null; auto_increment"`
@@ -14,4 +18,18 @@ type User struct {
 	Major         string `gorm:"type : varchar(50); not null"`
 	Create        int64  `gorm:"type : int(64); not null"`
 	LastLogin     int64  `gorm:"type : int(64); not null"`
+}
+
+// TurnProto : turn user to protobuf
+func (u *User) TurnProto() *protocol.UserInfo {
+	return &protocol.UserInfo{
+		Id:        u.ID,
+		Name:      u.Name,
+		Sex:       u.Sex,
+		Role:      protocol.Role(u.Role),
+		Academy:   u.Academy,
+		Major:     u.Major,
+		Create:    u.Create,
+		LastLogin: u.LastLogin,
+	}
 }
