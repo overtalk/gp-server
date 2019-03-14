b7af619d32d7dd7a0b7fc379be345f9e72b12ec8
秦翰 <qinhan@xindong.com>
2019-03-14 13:55:00 +0800

    feat(service/user_manage): add UpdateUsers and DelUsers interface

--- service/user_manage/manage.go
+++ service/user_manage/manage.go
@@ -120,3 +120,101 @@ func (m *UserManager) AddUsers(args map[string]interface{}) interface{} {
 
 	return resp
 }
+
+// UpdateUsers : update users
+func (m *UserManager) UpdateUsers(args map[string]interface{}) interface{} {
+	// get request and response
+	req := &protocol.UpdateUsersReq{}
+	resp := &protocol.UpdateUsersResp{}
+	if err := utils.CheckArgs(args, module.Request, module.Request); err != nil {
+		resp.Code = protocol.Code_INVAILD_DATA
+		return resp
+	}
+	if err := proto.Unmarshal(parse.Bytes(args[module.Request]), req); err != nil {
+		logger.Sugar.Errorf("failed to unmarshal : %v", err)
+		resp.Code = protocol.Code_INVAILD_DATA
+		return resp
+	}
+
+	// check token
+	userID, err := m.cache.GetUserIDByToken(parse.String(args[module.Token]))
+	if err != nil {
+		logger.Sugar.Errorf("invaild token : %v", err)
+		resp.Code = protocol.Code_INVAILD_TOKEN
+		return resp
+	}
+
+	// get user from db, and get the operation auth of the player
+	user, err := m.db.GetUserByID(userID)
+	if err != nil {
+		logger.Sugar.Errorf("failed to get user by id[%d] : %v", userID, err)
+		resp.Code = protocol.Code_PERMISSION_DENIED
+		return resp
+	}
+	if authCheck(user.OperationAuth) {
+		logger.Sugar.Errorf("failed to update users[permission denied] for user[id = %d, role = %s]", userID, protocol.Role_name[int32(user.Role)])
+		resp.Code = protocol.Code_PERMISSION_DENIED
+		return resp
+	}
+
+	// update users
+	for _, protoUser := range req.Users {
+		user := model.TurnUser(protoUser)
+		if err := m.db.UpdateUser(user); err != nil {
+			resp.Fail = append(resp.Fail, protoUser)
+		} else {
+			resp.Succeed = append(resp.Succeed, user.TurnProto())
+		}
+	}
+
+	return resp
+}
+
+// DelUsers : delete users
+func (m *UserManager) DelUsers(args map[string]interface{}) interface{} {
+	// get request and response
+	req := &protocol.UpdateUsersReq{}
+	resp := &protocol.UpdateUsersResp{}
+	if err := utils.CheckArgs(args, module.Request, module.Request); err != nil {
+		resp.Code = protocol.Code_INVAILD_DATA
+		return resp
+	}
+	if err := proto.Unmarshal(parse.Bytes(args[module.Request]), req); err != nil {
+		logger.Sugar.Errorf("failed to unmarshal : %v", err)
+		resp.Code = protocol.Code_INVAILD_DATA
+		return resp
+	}
+
+	// check token
+	userID, err := m.cache.GetUserIDByToken(parse.String(args[module.Token]))
+	if err != nil {
+		logger.Sugar.Errorf("invaild token : %v", err)
+		resp.Code = protocol.Code_INVAILD_TOKEN
+		return resp
+	}
+
+	// get user from db, and get the operation auth of the player
+	user, err := m.db.GetUserByID(userID)
+	if err != nil {
+		logger.Sugar.Errorf("failed to get user by id[%d] : %v", userID, err)
+		resp.Code = protocol.Code_PERMISSION_DENIED
+		return resp
+	}
+	if authCheck(user.OperationAuth) {
+		logger.Sugar.Errorf("failed to delete users[permission denied] for user[id = %d, role = %s]", userID, protocol.Role_name[int32(user.Role)])
+		resp.Code = protocol.Code_PERMISSION_DENIED
+		return resp
+	}
+
+	// delete users
+	for _, protoUser := range req.Users {
+		user := model.TurnUser(protoUser)
+		if err := m.db.DelUser(user); err != nil {
+			resp.Fail = append(resp.Fail, protoUser)
+		} else {
+			resp.Succeed = append(resp.Succeed, user.TurnProto())
+		}
+	}
+
+	return resp
+}
