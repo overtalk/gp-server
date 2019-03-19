45d7ad801f3ea7ba34aa84c43d0f2a66264a2ebe
秦翰 <qinhan@xindong.com>
2019-03-19 23:05:39 +0800

    feat(service/db/mysql): add where condition

---
+++ service/db/mysql/condition_test.go
@@ -0,0 +1,23 @@
+package db_test
+
+import (
+	"testing"
+
+	"github.com/qinhan-shu/gp-server/service/db/mysql"
+)
+
+func TestTurnMysqlWhere(t *testing.T) {
+	var conditions []db.Condition
+	conditions = append(conditions, db.Condition{
+		Filed:    "id",
+		Operator: "<",
+		Value:    1,
+	})
+	conditions = append(conditions, db.Condition{
+		Filed:    "name",
+		Operator: "=",
+		Value:    "sf",
+	})
+
+	t.Log(db.TurnMysqlWhere(conditions))
+}
