e0427afa6eafa81e68b08d3532620e5066e1977d
秦翰 <qinhan@xindong.com>
2019-04-23 11:12:08 +0800

    fix(all): fix a bug

--- server/file/server.go
+++ server/file/server.go
@@ -74,72 +74,75 @@ func uploadFileHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "x-requested-with") //header的类型
-		// validate file size
-		r.Body = http.MaxBytesReader(w, r.Body, maxSize)
-		if err := r.ParseMultipartForm(maxSize); err != nil {
-			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-			return
-		}
+		if r.Method == "POST" {
+			// validate file size
+			r.Body = http.MaxBytesReader(w, r.Body, maxSize)
+			if err := r.ParseMultipartForm(maxSize); err != nil {
+				renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+				return
+			}
 
-		// parse and validate file and post parameters
-		targetFile, header, err := r.FormFile("uploadFile")
-		if err != nil {
-			logger.Sugar.Errorf("%v", err)
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
-		defer targetFile.Close()
-		fileBytes, err := ioutil.ReadAll(targetFile)
-		if err != nil {
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
+			// parse and validate file and post parameters
+			targetFile, header, err := r.FormFile("uploadFile")
+			if err != nil {
+				logger.Sugar.Errorf("%v", err)
+				renderError(w, "INVALID_FILE", http.StatusBadRequest)
+				return
+			}
+			defer targetFile.Close()
+			fileBytes, err := ioutil.ReadAll(targetFile)
+			if err != nil {
+				renderError(w, "INVALID_FILE", http.StatusBadRequest)
+				return
+			}
 
-		// check file type, detectcontenttype only needs the first 512 bytes
-		filetype := http.DetectContentType(fileBytes)
-		switch filetype {
-		case "application/zip":
-			break
-		default:
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
+			// check file type, detectcontenttype only needs the first 512 bytes
+			filetype := http.DetectContentType(fileBytes)
+			switch filetype {
+			case "application/zip":
+				break
+			default:
+				renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+				return
+			}
 
-		// create a dir to store zip
-		results := strings.Split(header.Filename, ".")
-		if len(results) != 2 || results[1] != "zip" {
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
+			// create a dir to store zip
+			results := strings.Split(header.Filename, ".")
+			if len(results) != 2 || results[1] != "zip" {
+				renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+				return
+			}
 
-		dir := *uploadPath + results[0]
-		newPath := filepath.Join(dir, header.Filename)
+			dir := *uploadPath + results[0]
+			newPath := filepath.Join(dir, header.Filename)
 
-		err = os.Mkdir(dir, os.ModePerm)
-		if err != nil {
-			fmt.Println(err)
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
+			err = os.Mkdir(dir, os.ModePerm)
+			if err != nil {
+				fmt.Println(err)
+				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
 
-		// write file
-		newFile, err := os.Create(newPath)
-		if err != nil {
-			fmt.Println(err)
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		defer newFile.Close() // idempotent, okay to call twice
-		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
+			// write file
+			newFile, err := os.Create(newPath)
+			if err != nil {
+				fmt.Println(err)
+				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
+			defer newFile.Close() // idempotent, okay to call twice
+			if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
 
-		// unzip
-		if err := zip.Unzip(dir+"/"+header.Filename, dir); err != nil {
-			logger.Sugar.Error(err)
+			// unzip
+			if err := zip.Unzip(dir+"/"+header.Filename, dir); err != nil {
+				logger.Sugar.Error(err)
+			}
+			w.Write([]byte("ok, path = " + dir))
 		}
-		w.Write([]byte("ok, path = " + dir))
+		logger.Sugar.Info(r.Method)
 	})
 }
 
