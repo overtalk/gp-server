92116478d9e6e2912b6012114b1c54a9f4cb7181
秦翰 <qinhan@xindong.com>
2019-06-04 20:27:49 +0800

    refactor(utils): modify the method to get token

--- utils/http_tool.go
+++ utils/http_tool.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -16,15 +15,14 @@ var (
 
 // GetToken : get token from gin.Context
 func GetToken(r *http.Request) (string, error) {
-	// FIXME: remote next line
-	return tokenArgName, nil
-	cookies := r.Cookies()
-	for _, cookie := range cookies {
-		if cookie.Name == tokenArgName {
-			return cookie.Value, nil
-		}
-	}
-	return "", errors.New("no token")
+	//cookies := r.Cookies()
+	//for _, cookie := range cookies {
+	//	if cookie.Name == tokenArgName {
+	//		return cookie.Value, nil
+	//	}
+	//}
+	//return "", errors.New("no token")
+	return r.URL.Query().Get("token"),nil
 }
 
 // GetRequestBody : get request body
